66322483dab02550e67d62fca597f9f396cc6bf2
serge.klochkov <hypnoash@gmail.com>
2022-04-29 23:17:37 +0200

    Init NATS listeners a bit differently

--- src/startup.rs
+++ src/startup.rs
@@ -1,8 +1,9 @@
 use std::net::TcpListener;
 
+use crate::config::CONFIG;
 use crate::db::subscription_queries::SubscriptionQueries;
 use crate::email_client::EmailClient;
-use crate::init_listeners::init_listeners;
+use crate::events::subscription_created::SubscriptionCreated;
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
@@ -25,14 +26,23 @@ pub fn run(
     let subscription_queries_data =
         web::Data::new(SubscriptionQueries::new(pg_pool_data.into_inner()));
     let subscription_queries_data_clone = subscription_queries_data.clone();
-    tokio::spawn(async move {
-        init_listeners(
-            &nats_connection_data_clone,
-            &subscription_queries_data_clone,
-            &email_client_data_clone,
-        )
-        .await
-        .expect("Failed to init NATS listeners");
+
+    let _ = tokio::spawn(async move {
+        let sub_created = nats_connection_data_clone
+            .queue_subscribe(
+                &CONFIG.nats_subscription_created_subject,
+                &CONFIG.nats_subscription_created_group,
+            )
+            .await
+            .expect("Failed to subscribe to SubscriptionCreated subject");
+        if let Some(msg) = sub_created.next().await {
+            let _ = SubscriptionCreated::process(
+                &email_client_data_clone,
+                &subscription_queries_data_clone,
+                msg,
+            )
+            .await;
+        }
     });
 
     let server = HttpServer::new(move || {
